6a9a4b4cae5377cd217f9b745e543b6cb3e323d6
uditsharma2501 <udit.sharma@unthinkable.co>
2024-05-28 04:14:14 +0530

    - Updated the base file to run with chrome container over docker

--- src/test/java/maven_demo/Base.java
+++ src/test/java/maven_demo/Base.java
@@ -1,7 +1,12 @@
 package maven_demo;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeSuite;
 
@@ -12,13 +17,32 @@ public class Base {
 	public static WebDriverWait wait;
 	
 	@BeforeSuite
-	public void initialize()
+	public void initialize() throws MalformedURLException
 	{
-		System.setProperty("webdriver.gecko.driver","C:\\Users\\Lap-1100\\Documents\\Selenium\\Gecko Driver\\geckodriver.exe");
+		String host="localhost";
 		
-		driver=new FirefoxDriver();
+		if(System.getProperty("HUB_HOST")!=null)
+		{
+			host=System.getProperty("HUB_HOST");
+		}
 		
-		wait=new WebDriverWait(driver,300);
+		String completeURL="http://" + host + ":4545/wd/hub";
+		
+		DesiredCapabilities dc=null;
+		
+		if(System.getProperty("BROWSER")!=null && System.getProperty("BROWSER").equalsIgnoreCase("firefox"))
+		{
+			dc=DesiredCapabilities.firefox();
+		}
+		
+		dc=DesiredCapabilities.chrome();
+		this.driver= new RemoteWebDriver(new URL(completeURL), dc);
+		
+//		System.setProperty("webdriver.gecko.driver","C:\\Users\\Lap-1100\\Documents\\Selenium\\Gecko Driver\\geckodriver.exe");
+//		
+//		driver=new FirefoxDriver();
+//		
+//		wait=new WebDriverWait(driver,300);
 		
 		String demo_URL="https://demo.applitools.com/";
 		
